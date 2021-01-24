257b1587aca8761a54e685bccfcce826baed665b
Maxim Vainikka <vainikka@users.noreply.github.com>
2021-01-24 10:28:07 +1000

    pog

---
+++ draw/OtherView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherView.swift
+//  draw
+//
+//  Created by Максим on 19.01.2021.
+//
+
+import SwiftUI
+
+struct OtherView: View {
+    var body: some View {
+        Text("Meine greetings")
+    }
+}
+
+struct OtherView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherView()
+    }
+}
